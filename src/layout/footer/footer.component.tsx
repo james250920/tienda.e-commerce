2590355b4d3f4fc7f4d79ecfb8c17756422dd3da
JAMES FRANK MENDOZA RIOS <21100654@ue.edu.pe>
2025-08-01 05:30:51 -0500

    feat: add header and footer components with basic structure

---
+++ src/layout/footer/footer.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function FooterComponent() {
+  return (
+    <footer>
+      <h1>Footer Content</h1>
+    </footer>
+  )
+}
+
+
---
+++ src/layout/header/header.component.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HeaderComponent() {
+  return (
+    <header>
+      <h1>desde el header</h1>
+    </header>
+  )
+}
+
+
